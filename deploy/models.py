86cebf9ac7c783fec15502faf2b48dfc539d1327
iskandar7171 <tolagankhojaeviskandar@gmail.com>
2026-02-27 16:05:45 +0300

    upload

---
+++ deploy/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Student(models.Model):
+    ism = models.CharField(max_length=255)
+    yosh = models.SmallIntegerField(default=0)
+    bio = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.ism}"
